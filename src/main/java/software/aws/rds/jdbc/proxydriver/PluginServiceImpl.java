01f7d8f67a54a0c4b784f12c2361f6e1caffb674
Karen <64801825+karenc-bq@users.noreply.github.com>
2022-06-13 10:35:11 -0700

    feat: setNodeListProvider (#11)
    feat: setHostListProvider

--- src/main/java/software/aws/rds/jdbc/proxydriver/PluginServiceImpl.java
+++ src/main/java/software/aws/rds/jdbc/proxydriver/PluginServiceImpl.java
@@ -11,76 +11,86 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
+import software.aws.rds.jdbc.proxydriver.nodelistprovider.ConnectionStringHostListProvider;
 
 public class PluginServiceImpl implements PluginService, HostListProviderService {
 
-    protected final ConnectionPluginManager pluginManager;
-    protected HostSpec[] hostSpecs;
-    protected Connection currentConnection;
-    protected HostSpec currentHostSpec;
+  protected final ConnectionPluginManager pluginManager;
+  protected volatile HostListProvider hostListProvider;
 
-    public PluginServiceImpl(@NonNull ConnectionPluginManager pluginManager) {
-        this.pluginManager = pluginManager;
+  protected HostSpec[] hostSpecs;
+  protected Connection currentConnection;
+  protected HostSpec currentHostSpec;
+
+  public PluginServiceImpl(@NonNull ConnectionPluginManager pluginManager) {
+    this.pluginManager = pluginManager;
+  }
+
+  @Override
+  public Connection getCurrentConnection() {
+    return this.currentConnection;
+  }
+
+  @Override
+  public HostSpec getCurrentHostSpec() {
+    return this.currentHostSpec;
+  }
+
+  @Override
+  public void setCurrentConnection(final @NonNull Connection connection, final @NonNull HostSpec hostSpec) {
+    this.currentConnection = connection;
+    this.currentHostSpec = hostSpec;
+  }
+
+  @Override
+  public HostSpec[] getHosts() {
+    return new HostSpec[0];
+  }
+
+  @Override
+  public boolean isExplicitReadOnly() {
+    return false;
+  }
+
+  @Override
+  public boolean isReadOnly() {
+    return false;
+  }
+
+  @Override
+  public boolean isInTransaction() {
+    return false;
+  }
+
+  @Override
+  public HostListProvider getHostListProvider() {
+    if (this.hostListProvider == null) {
+      synchronized (this) {
+        if (this.hostListProvider == null) {
+          this.hostListProvider = new ConnectionStringHostListProvider();
+        }
+      }
     }
+    return this.hostListProvider;
+  }
 
-    @Override
-    public Connection getCurrentConnection() {
-        return this.currentConnection;
-    }
+  @Override
+  public void refreshHostList() {
 
-    @Override
-    public HostSpec getCurrentHostSpec() {
-        return this.currentHostSpec;
-    }
+  }
 
-    @Override
-    public void setCurrentConnection(final @NonNull Connection connection, final @NonNull HostSpec hostSpec) {
-        this.currentConnection = connection;
-        this.currentHostSpec = hostSpec;
-    }
+  @Override
+  public boolean isDefaultHostListProvider() {
+    return this.hostListProvider instanceof ConnectionStringHostListProvider;
+  }
 
-    @Override
-    public HostSpec[] getHosts() {
-        return new HostSpec[0];
-    }
+  @Override
+  public void setHostListProvider(HostListProvider hostListProvider) {
+    this.hostListProvider = hostListProvider;
+  }
 
-    @Override
-    public boolean isExplicitReadOnly() {
-        return false;
-    }
-
-    @Override
-    public boolean isReadOnly() {
-        return false;
-    }
-
-    @Override
-    public boolean isInTransaction() {
-        return false;
-    }
-
-    @Override
-    public HostListProvider getHostListProvider() {
-        return null;
-    }
-
-    @Override
-    public void refreshHostList() {
-
-    }
-
-    @Override
-    public boolean isDefaultHostListProvider() {
-        return false;
-    }
-
-    @Override
-    public void setHostListProvider(HostListProvider hostListProvider) {
-
-    }
-
-    @Override
-    public Connection connect(HostSpec hostSpec, Properties props) throws SQLException {
-        return null;
-    }
+  @Override
+  public Connection connect(HostSpec hostSpec, Properties props) throws SQLException {
+    return null;
+  }
 }
