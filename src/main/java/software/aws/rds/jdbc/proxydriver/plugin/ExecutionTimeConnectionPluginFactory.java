07ca71fcc770d60ee0675c9e8f73e33532690e86
Karen <64801825+karenc-bq@users.noreply.github.com>
2022-06-21 09:29:12 -0700

    chore: update copyright headers (#28)

--- src/main/java/software/aws/rds/jdbc/proxydriver/plugin/ExecutionTimeConnectionPluginFactory.java
+++ src/main/java/software/aws/rds/jdbc/proxydriver/plugin/ExecutionTimeConnectionPluginFactory.java
@@ -1,9 +1,7 @@
 /*
- *
- *  * AWS JDBC Proxy Driver
- *  * Copyright Amazon.com Inc. or affiliates.
- *  * See the LICENSE file in the project root for more information.
- *
+ * AWS JDBC Proxy Driver
+ * Copyright Amazon.com Inc. or affiliates.
+ * See the LICENSE file in the project root for more information.
  */
 
 package software.aws.rds.jdbc.proxydriver.plugin;
