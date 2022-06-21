b9c85bb0ffe3a6ee459b39b7aa8af894b325d941
crystall-bitquill <97126568+crystall-bitquill@users.noreply.github.com>
2022-06-21 10:28:58 -0700

    Token Substitute for DriverInfo (#27)
    * chore: add driver version substitution, edit gitignore to cover any .gradle or build folder
    * Update license header comment

--- src/main/version/software/aws/rds/jdbc/proxydriver/util/DriverInfo.java
+++ src/main/version/software/aws/rds/jdbc/proxydriver/util/DriverInfo.java
@@ -8,20 +8,20 @@ package software.aws.rds.jdbc.proxydriver.util;
 
 public final class DriverInfo {
 
-    private DriverInfo() {
-    }
+  // Driver name
+  public static final String DRIVER_NAME = "Amazon Web Services (AWS) JDBC Proxy Driver";
+  public static final String DRIVER_VERSION = "/*$version$*/";
 
-    // Driver name
-    public static final String DRIVER_NAME = "Amazon Web Services (AWS) JDBC Proxy Driver";
-    public static final String DRIVER_VERSION = "/*$version$*/"; //TODO: substitute
+  // Driver version
+  public static final int MAJOR_VERSION = /*$version.major+";"$*//*-*/0;
+  public static final int MINOR_VERSION = /*$version.minor+";"$*//*-*/1;
+  public static final int PATCH_VERSION = /*$version.patch+";"$*//*-*/0;
 
-    // Driver version
-    public static final int MAJOR_VERSION = 0;
-    public static final int MINOR_VERSION = 1;
-    public static final int PATCH_VERSION = 0;
+  // JDBC specification
+  public static final String JDBC_VERSION = "4.2";
+  public static final int JDBC_MAJOR_VERSION = JDBC_VERSION.charAt(0) - '0';
+  public static final int JDBC_MINOR_VERSION = JDBC_VERSION.charAt(2) - '0';
 
-    // JDBC specification
-    public static final String JDBC_VERSION = "4.2";
-    public static final int JDBC_MAJOR_VERSION = JDBC_VERSION.charAt(0) - '0';
-    public static final int JDBC_MINOR_VERSION = JDBC_VERSION.charAt(2) - '0';
+  private DriverInfo() {
+  }
 }
