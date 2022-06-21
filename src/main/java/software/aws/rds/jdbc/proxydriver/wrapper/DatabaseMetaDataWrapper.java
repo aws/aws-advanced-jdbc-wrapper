a35fb3cc5e817882aee5020a5d195e4300b913fe
Karen <64801825+karenc-bq@users.noreply.github.com>
2022-06-21 09:29:12 -0700

    chore: update copyright headers (#28)

--- src/main/java/software/aws/rds/jdbc/proxydriver/wrapper/DatabaseMetaDataWrapper.java
+++ src/main/java/software/aws/rds/jdbc/proxydriver/wrapper/DatabaseMetaDataWrapper.java
@@ -1,3 +1,9 @@
+/*
+ * AWS JDBC Proxy Driver
+ * Copyright Amazon.com Inc. or affiliates.
+ * See the LICENSE file in the project root for more information.
+ */
+
 package software.aws.rds.jdbc.proxydriver.wrapper;
 
 import java.sql.Connection;
