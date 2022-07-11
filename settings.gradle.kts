31df2b026676a25827552a8f8afe23dbc844d346
sergiyv-bitquill <69876824+sergiyv-bitquill@users.noreply.github.com>
2022-07-11 10:37:47 -0700

    update dependencies versions (#46)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,12 +17,12 @@ pluginManagement {
         fun String.v() = extra["$this.version"].toString()
         fun PluginDependenciesSpec.idv(id: String, key: String = id) = id(id) version key.v()
 
-        id("com.github.spotbugs") version "5.0.8"
-        id("com.diffplug.spotless") version "6.8.0"
-        id("com.github.vlsi.gradle-extensions") version "1.82"
-        id("com.github.vlsi.stage-vote-release") version "1.82"
-        id("com.github.vlsi.ide") version "1.82"
-        id("me.champeau.gradle.jmh") version "0.5.3"
-        id("org.checkerframework") version "0.5.5"
+        id("com.github.spotbugs") version "5.0.+"
+        id("com.diffplug.spotless") version "6.8.+"
+        id("com.github.vlsi.gradle-extensions") version "1.+"
+        id("com.github.vlsi.stage-vote-release") version "1.+"
+        id("com.github.vlsi.ide") version "1.+"
+        id("me.champeau.gradle.jmh") version "0.+"
+        id("org.checkerframework") version "0.+"
     }
 }
