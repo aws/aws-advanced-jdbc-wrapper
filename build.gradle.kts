8c9c15e95b9ffbf45080a379f489cd7748100997
alecc <alecc@bitquilltech.com>
2022-08-18 12:55:07 -0700

    change repository uri depending on if snapshot release or not

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,7 +113,11 @@ allprojects {
         repositories {
             maven {
                 name = "OSSRH"
-                url = uri("https://aws.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                url = if(project.property("snapshot") == true) {
+                    uri("https://aws.oss.sonatype.org/content/repositories/snapshots/")
+                } else {
+                    uri("https://aws.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                }
                 credentials {
                     username = System.getenv("MAVEN_USERNAME")
                     password = System.getenv("MAVEN_PASSWORD")
