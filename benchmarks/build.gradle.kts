f9713387a2699275bb73db0c36ce62de2684b6ea
Karen Chen <karenc@bitquilltech.com>
2022-09-06 17:07:39 -0700

    chore: add benchmarking subproject

---
+++ benchmarks/build.gradle.kts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("me.champeau.jmh")
+}
+
+dependencies {
+    jmhImplementation(project(":aws-advanced-jdbc-wrapper"))
+    implementation("org.postgresql:postgresql:42.+")
+    implementation("mysql:mysql-connector-java:8.0.+")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.+")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testImplementation("org.mockito:mockito-inline:4.+")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
