b56983f577a8c8f7b2df81bb14b6ef63963f2553
Karen <64801825+karenc-bq@users.noreply.github.com>
2022-10-07 12:52:55 -0700

    refactor: cache host status (#210)
    Cache host availability so new topology information contains the correct host availability

---
+++ wrapper/src/test/java/integration/container/aurora/TestPluginServiceImpl.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package integration.container.aurora;
+
+import java.util.Properties;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import software.amazon.jdbc.ConnectionPluginManager;
+import software.amazon.jdbc.PluginServiceImpl;
+
+public class TestPluginServiceImpl extends PluginServiceImpl {
+
+  public TestPluginServiceImpl(
+      @NonNull ConnectionPluginManager pluginManager,
+      @NonNull Properties props,
+      @NonNull String originalUrl, String targetDriverProtocol) {
+    super(pluginManager, props, originalUrl, targetDriverProtocol);
+  }
+
+  public static void clearHostAvailabilityCache() {
+    PluginServiceImpl.hostAvailabilityExpiringCache.clear();
+  }
+}
