44eee5a31ae4000b3382e1a83aa1c9839e76e4ee
alecc <alecc@bitquilltech.com>
2022-10-26 10:23:14 -0700

    removed mariadb specific params from table, added explanation to the sample code

--- examples/AWSDriverExample/src/main/java/software/amazon/AwsIamAuthenticationMariadbExample.java
+++ examples/AWSDriverExample/src/main/java/software/amazon/AwsIamAuthenticationMariadbExample.java
@@ -37,7 +37,12 @@ public class AwsIamAuthenticationMariadbExample {
     // Enable AWS IAM database authentication and configure driver property values
     properties.setProperty(PropertyDefinition.USER.name, USERNAME);
     properties.setProperty(PropertyDefinition.PLUGINS.name, "iam");
+
+    // The following two properties are required when using the IAM Authentication plugin with the MariaDB driver.
     properties.setProperty("sslMode", "verify-ca");
+
+    // The value of this property should be the path to an SSL certificate.
+    // The certificates can be found here: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL.html.
     properties.setProperty("serverSslCert", "path/to/certificate.pem");
 
     // Attempt a connection
