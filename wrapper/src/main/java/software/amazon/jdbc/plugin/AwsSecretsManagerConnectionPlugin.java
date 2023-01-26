e4ce4137678abdd6e030f0624fc1e1aec5cf0657
Karen <64801825+karenc-bq@users.noreply.github.com>
2023-01-26 13:31:05 -0800

    fix: close aws secrets manager client (#321)
    Close SecretsManagerClient to prevent leaking PoolingHttpClientConnectionManager.

--- wrapper/src/main/java/software/amazon/jdbc/plugin/AwsSecretsManagerConnectionPlugin.java
+++ wrapper/src/main/java/software/amazon/jdbc/plugin/AwsSecretsManagerConnectionPlugin.java
@@ -28,6 +28,8 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import software.amazon.awssdk.regions.Region;
@@ -58,8 +60,9 @@ public class AwsSecretsManagerConnectionPlugin extends AbstractConnectionPlugin 
   protected static final Map<Pair<String, Region>, Secret> secretsCache = new ConcurrentHashMap<>();
 
   private final Pair<String, Region> secretKey;
-  private SecretsManagerClient secretsManagerClient;
-  private GetSecretValueRequest getSecretValueRequest;
+  private final BiFunction<HostSpec, Region, SecretsManagerClient>
+      secretsManagerClientFunc;
+  private final Function<String, GetSecretValueRequest> getSecretValueRequestFunc;
   private Secret secret;
   protected PluginService pluginService;
 
@@ -68,16 +71,21 @@ public class AwsSecretsManagerConnectionPlugin extends AbstractConnectionPlugin 
     this(
         pluginService,
         props,
-        null,
-        null
+        (hostSpec, region) -> SecretsManagerClient.builder()
+            .credentialsProvider(AwsCredentialsManager.getProvider(hostSpec, props))
+            .region(region)
+            .build(),
+        (secretId) -> GetSecretValueRequest.builder()
+            .secretId(secretId)
+            .build()
     );
   }
 
   AwsSecretsManagerConnectionPlugin(
       PluginService pluginService,
       Properties props,
-      SecretsManagerClient secretsManagerClient,
-      GetSecretValueRequest getSecretValueRequest) {
+      BiFunction<HostSpec, Region, SecretsManagerClient> secretsManagerClientFunc,
+      Function<String, GetSecretValueRequest> getSecretValueRequestFunc) {
     this.pluginService = pluginService;
 
     try {
@@ -117,10 +125,8 @@ public class AwsSecretsManagerConnectionPlugin extends AbstractConnectionPlugin 
     }
     this.secretKey = Pair.of(secretId, region);
 
-    if (secretsManagerClient != null && getSecretValueRequest != null) {
-      this.secretsManagerClient = secretsManagerClient;
-      this.getSecretValueRequest = getSecretValueRequest;
-    }
+    this.secretsManagerClientFunc = secretsManagerClientFunc;
+    this.getSecretValueRequestFunc = getSecretValueRequestFunc;
   }
 
   @Override
@@ -137,17 +143,7 @@ public class AwsSecretsManagerConnectionPlugin extends AbstractConnectionPlugin 
       final JdbcCallable<Connection, SQLException> connectFunc)
       throws SQLException {
 
-    if (this.secretsManagerClient == null || this.getSecretValueRequest == null) {
-      this.secretsManagerClient = SecretsManagerClient.builder()
-          .credentialsProvider(AwsCredentialsManager.getProvider(hostSpec, props))
-          .region(this.secretKey.right())
-          .build();
-      this.getSecretValueRequest = GetSecretValueRequest.builder()
-          .secretId(this.secretKey.left())
-          .build();
-    }
-
-    boolean secretWasFetched = updateSecret(false);
+    boolean secretWasFetched = updateSecret(hostSpec, false);
 
     try {
       applySecretToProperties(props);
@@ -158,7 +154,7 @@ public class AwsSecretsManagerConnectionPlugin extends AbstractConnectionPlugin 
         // Login unsuccessful with cached credentials
         // Try to re-fetch credentials and try again
 
-        secretWasFetched = updateSecret(true);
+        secretWasFetched = updateSecret(hostSpec, true);
         if (secretWasFetched) {
           applySecretToProperties(props);
           return connectFunc.call();
@@ -181,14 +177,14 @@ public class AwsSecretsManagerConnectionPlugin extends AbstractConnectionPlugin 
    * @param forceReFetch Allows ignoring cached credentials and force fetches the latest credentials from the service.
    * @return true, if credentials were fetched from the service.
    */
-  private boolean updateSecret(boolean forceReFetch) throws SQLException {
+  private boolean updateSecret(final HostSpec hostSpec, boolean forceReFetch) throws SQLException {
 
     boolean fetched = false;
     this.secret = secretsCache.get(this.secretKey);
 
     if (secret == null || forceReFetch) {
       try {
-        this.secret = fetchLatestCredentials();
+        this.secret = fetchLatestCredentials(hostSpec);
         if (this.secret != null) {
           fetched = true;
           secretsCache.put(this.secretKey, this.secret);
@@ -208,12 +204,25 @@ public class AwsSecretsManagerConnectionPlugin extends AbstractConnectionPlugin 
   /**
    * Fetches the current credentials from AWS Secrets Manager service.
    *
+   * @param hostSpec A {@link HostSpec} instance containing host information for the current connection.
    * @return a Secret object containing the credentials fetched from the AWS Secrets Manager service.
    * @throws SecretsManagerException if credentials can't be fetched from the AWS Secrets Manager service.
    * @throws JsonProcessingException if credentials can't be mapped to a Secret object.
    */
-  Secret fetchLatestCredentials() throws SecretsManagerException, JsonProcessingException {
-    final GetSecretValueResponse valueResponse = this.secretsManagerClient.getSecretValue(this.getSecretValueRequest);
+  Secret fetchLatestCredentials(final HostSpec hostSpec)
+      throws SecretsManagerException, JsonProcessingException {
+    final SecretsManagerClient client = secretsManagerClientFunc.apply(
+        hostSpec,
+        this.secretKey.right());
+    final GetSecretValueRequest request = getSecretValueRequestFunc.apply(this.secretKey.left());
+
+    final GetSecretValueResponse valueResponse;
+    try {
+      valueResponse = client.getSecretValue(request);
+    } finally {
+      client.close();
+    }
+
     final ObjectMapper mapper = new ObjectMapper();
     return mapper.readValue(valueResponse.secretString(), Secret.class);
   }
