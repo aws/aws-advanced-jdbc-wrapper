e7d6b3dfdc4ddd15649d1bc5f342c52d76abb15f
Aaron Congo <Aaron.Congo@improving.com>
2023-02-27 10:31:42 -0800

    Allow selection of specific test classes/methods using test filters

--- wrapper/src/test/build.gradle.kts
+++ wrapper/src/test/build.gradle.kts
@@ -81,5 +81,10 @@ tasks.withType<Test> {
 }
 
 tasks.register<Test>("in-container") {
+    filter.excludeTestsMatching("software.*") // exclude unit tests
+    filter.excludeTestsMatching("integration.container.*") // exclude old integration tests
+
+    // modify below filter to select specific integration tests
+    // see https://docs.gradle.org/current/javadoc/org/gradle/api/tasks/testing/TestFilter.html
     filter.includeTestsMatching("integration.refactored.container.tests.*")
 }
