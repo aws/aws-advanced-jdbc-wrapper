86ce9428fa48686d0ce00c3ac4dc8a8c767c4614
sergiyv-bitquill <sergiyv@bitquilltech.com>
2023-05-25 12:44:05 -0700

    code style

--- wrapper/src/test/java/software/amazon/logging/ExtendedFormatter.java
+++ wrapper/src/test/java/software/amazon/logging/ExtendedFormatter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package software.amazon.logging;
 
 import java.io.PrintWriter;
@@ -15,7 +31,7 @@ import java.util.logging.SimpleFormatter;
 public class ExtendedFormatter extends SimpleFormatter {
 
   /**
-   * Format string arguments:
+   * Format string arguments are presented below.
    * - ( 1$) the timestamp
    * - ( 2$) the source
    * - ( 3$) the logger name
