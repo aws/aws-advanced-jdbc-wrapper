fac186bb4f91252c119a846ebe265ed11a8b484a
sergiyv-bitquill <sergiyv@bitquilltech.com>
2023-06-02 15:28:21 -0700

    logging

--- wrapper/src/main/java/software/amazon/jdbc/targetdriverdialect/TargetDriverDialectManager.java
+++ wrapper/src/main/java/software/amazon/jdbc/targetdriverdialect/TargetDriverDialectManager.java
@@ -75,7 +75,6 @@ public class TargetDriverDialectManager implements TargetDriverDialectProvider {
       final @NonNull String dataSourceClass,
       final @NonNull Properties props) throws SQLException {
 
-    LOGGER.finest(() -> "Try to identify target driver dialect for data source class: " + dataSourceClass);
     return this.getDialect(props, (targetDriverDialect -> targetDriverDialect.isDialect(dataSourceClass)));
   }
 
