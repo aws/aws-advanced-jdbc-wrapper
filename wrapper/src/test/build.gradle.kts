74a1e435be59c476f4d925965ca5bb1e44b29be0
sergiyvamz <svvoshch@amazon.com>
2023-07-18 16:46:03 -0700

    upgrade to 4.11.0

--- wrapper/src/test/build.gradle.kts
+++ wrapper/src/test/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     testImplementation("org.mariadb.jdbc:mariadb-java-client:3.1.0")
     testImplementation("com.zaxxer:HikariCP:4.+") // version 4.+ is compatible with Java 8
     testImplementation("org.springframework.boot:spring-boot-starter-jdbc:2.7.13") // 2.7.13 is the last version compatible with Java 8
-    testImplementation("org.mockito:mockito-inline:4.8.0")
+    testImplementation("org.mockito:mockito-inline:4.11.0") // 4.11.0 is the last version compatible with Java 8
     testImplementation("software.amazon.awssdk:rds:2.20.49")
     testImplementation("software.amazon.awssdk:ec2:2.20.49")
     testImplementation("org.testcontainers:testcontainers:1.17.+")
