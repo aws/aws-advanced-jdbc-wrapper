de360518a11992cf7c782ff331cf842221eaa657
Fang Li <fangli@users.noreply.github.com>
2023-09-28 22:25:22 -0700

    fix: RdsMultiAzDbClusterPgDialect with correct base class

--- wrapper/src/main/java/software/amazon/jdbc/dialect/RdsMultiAzDbClusterPgDialect.java
+++ wrapper/src/main/java/software/amazon/jdbc/dialect/RdsMultiAzDbClusterPgDialect.java
@@ -23,7 +23,7 @@ import java.sql.Statement;
 import java.util.List;
 import software.amazon.jdbc.hostlistprovider.RdsMultiAzDbClusterListProvider;
 
-public class RdsMultiAzDbClusterPgDialect extends MysqlDialect {
+public class RdsMultiAzDbClusterPgDialect extends PgDialect {
 
   private static final String TOPOLOGY_QUERY =
       "SELECT LOWER(id) AS id, LOWER(endpoint) AS endpoint, port FROM rds_tools.show_topology()";
