47a8d413bb54e31224a922041e2ed4f062f205c9
Crystal Liang <crystal.liang@improving.com>
2023-10-26 14:50:58 -0700

    chore: update aws sdk dependency versions

--- wrapper/build.gradle.kts
+++ wrapper/build.gradle.kts
@@ -28,9 +28,9 @@ plugins {
 
 dependencies {
     implementation("org.checkerframework:checker-qual:3.39.0")
-    compileOnly("software.amazon.awssdk:rds:2.21.7")
+    compileOnly("software.amazon.awssdk:rds:2.21.10")
     compileOnly("com.zaxxer:HikariCP:4.0.3") // Version 4.+ is compatible with Java 8
-    compileOnly("software.amazon.awssdk:secretsmanager:2.21.5")
+    compileOnly("software.amazon.awssdk:secretsmanager:2.21.10")
     compileOnly("com.fasterxml.jackson.core:jackson-databind:2.15.3")
     compileOnly("mysql:mysql-connector-java:8.0.33")
     compileOnly("org.postgresql:postgresql:42.6.0")
@@ -58,9 +58,9 @@ dependencies {
     testImplementation("com.zaxxer:HikariCP:4.0.3") // Version 4.+ is compatible with Java 8
     testImplementation("org.springframework.boot:spring-boot-starter-jdbc:2.7.13") // 2.7.13 is the last version compatible with Java 8
     testImplementation("org.mockito:mockito-inline:4.11.0") // 4.11.0 is the last version compatible with Java 8
-    testImplementation("software.amazon.awssdk:rds:2.21.7")
-    testImplementation("software.amazon.awssdk:ec2:2.21.3")
-    testImplementation("software.amazon.awssdk:secretsmanager:2.21.5")
+    testImplementation("software.amazon.awssdk:rds:2.21.10")
+    testImplementation("software.amazon.awssdk:ec2:2.21.10")
+    testImplementation("software.amazon.awssdk:secretsmanager:2.21.10")
     testImplementation("org.testcontainers:testcontainers:1.19.1")
     testImplementation("org.testcontainers:mysql:1.19.1")
     testImplementation("org.testcontainers:postgresql:1.19.1")
