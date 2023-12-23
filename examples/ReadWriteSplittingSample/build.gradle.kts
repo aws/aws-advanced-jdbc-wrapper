1ea20bbc978f893b89610026130e92bd56a8d669
congoamz <75754763+congoamz@users.noreply.github.com>
2023-12-22 16:52:57 -0800

    Add example code for Read/Write Splitting sample (#765)

---
+++ examples/ReadWriteSplittingSample/build.gradle.kts
@@ -0,0 +1,26 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+dependencies {
+    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("com.zaxxer:HikariCP:4.0.3")
+    implementation(project(":aws-advanced-jdbc-wrapper"))
+}
+
+tasks.withType<JavaExec> {
+    systemProperty("java.util.logging.config.file", "${project.buildDir}/resources/main/logging.properties")
+}
