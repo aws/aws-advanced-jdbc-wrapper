1245bf371eff4dd8f1ff5e5626ee02d68f567c59
crystall-bitquill <97126568+crystall-bitquill@users.noreply.github.com>
2024-02-07 17:38:03 -0800

    fix: statement cancel not working with mysql driver (#851)

---
+++ wrapper/src/main/java/software/amazon/jdbc/util/AsynchronousMethodsHelper.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package software.amazon.jdbc.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AsynchronousMethodsHelper {
+  public static final List<String> ASYNCHRONOUS_METHODS = Arrays.asList(
+      "Statement.cancel",
+      "PreparedStatement.cancel"
+  );
+}
