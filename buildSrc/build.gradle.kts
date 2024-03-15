4b91be1f783d58666f9875925be706e34dc9ad51
jasonlamz <lijjaso@amazon.com>
2024-03-15 15:09:01 -0700

    8.6 gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -20,6 +20,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `kotlin-dsl`
+    java
 }
 
 repositories {
@@ -29,6 +30,13 @@ repositories {
 
 dependencies {
     implementation("com.igormaznitsa:jcp:7.0.5")
+}
+
+tasks {
+    compileJava {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+    }
 }
 
 tasks.withType<KotlinCompile> {
