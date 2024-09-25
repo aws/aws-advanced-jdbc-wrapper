4f70fe63e679c31d1fc186b4520d337fb34ecc87
aaron-congo <aaron.congo@improving.com>
2024-09-25 09:13:43 -0700

    Cleaned up test setup/teardown

--- wrapper/src/test/java/integration/container/tests/CustomEndpointTest.java
+++ wrapper/src/test/java/integration/container/tests/CustomEndpointTest.java
@@ -19,6 +19,7 @@ import integration.util.AuroraTestUtility;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -34,7 +35,9 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rds.RdsClient;
+import software.amazon.awssdk.services.rds.model.CreateDbClusterEndpointResponse;
 import software.amazon.awssdk.services.rds.model.DBClusterEndpoint;
+import software.amazon.awssdk.services.rds.model.DbClusterEndpointNotFoundException;
 import software.amazon.awssdk.services.rds.model.DescribeDbClusterEndpointsResponse;
 import software.amazon.awssdk.services.rds.model.Filter;
 import software.amazon.jdbc.PropertyDefinition;
@@ -52,96 +55,124 @@ import software.amazon.jdbc.plugin.failover.FailoverSuccessSQLException;
 @Order(16)
 public class CustomEndpointTest {
   private static final Logger LOGGER = Logger.getLogger(CustomEndpointTest.class.getName());
-  protected static final String customEndpointId = "test-endpoint-1";
-  protected static DBClusterEndpoint endpointInfo;
+  protected static final String oneInstanceEndpointId = "test-endpoint-1";
+  protected static final String twoInstanceEndpointId = "test-endpoint-2";
+  protected static final List<String> endpointIDs = Arrays.asList(oneInstanceEndpointId, twoInstanceEndpointId);
+  protected static CreateDbClusterEndpointResponse oneInstanceEndpointInfo;
+  protected static CreateDbClusterEndpointResponse twoInstanceEndpointInfo;
   protected static final AuroraTestUtility auroraUtil = AuroraTestUtility.getUtility();
 
   protected String currentWriter;
 
   @BeforeAll
-  public static void createCustomEndpoints() {
+  public static void createEndpoints() {
     TestEnvironmentInfo envInfo = TestEnvironment.getCurrent().getInfo();
     String region = envInfo.getRegion();
-    List<TestInstanceInfo> instances = envInfo.getDatabaseInfo().getInstances();
+
     try (RdsClient client = RdsClient.builder().region(Region.of(region)).build()) {
-//       // Delete pre-existing custom endpoint in case it wasn't cleaned up in a previous run.
-//       try {
-//         client.deleteDBClusterEndpoint((builder) -> builder.dbClusterEndpointIdentifier(customEndpointId));
-//         long deleteTimeoutNano = System.nanoTime() + TimeUnit.MINUTES.toNanos(5);
-//         boolean deleted = false;
-//
-//         // TODO: should this be moved outside the try-catch? If the cleanup from the last test run sent a
-//         //  delete-endpoint request but it hasn't completed yet, we need to wait until it has.
-//         while (!deleted && System.nanoTime() < deleteTimeoutNano) {
-//           Filter customEndpointFilter =
-//               Filter.builder().name("db-cluster-endpoint-type").values("custom").build();
-//           DescribeDbClusterEndpointsResponse endpointsResponse = client.describeDBClusterEndpoints(
-//               (builder) ->
-//                   builder.dbClusterEndpointIdentifier(customEndpointId).filters(customEndpointFilter));
-//           deleted = endpointsResponse.dbClusterEndpoints().isEmpty();
-//         }
-//
-//         if (!deleted) {
-//           throw new RuntimeException(
-//               "The test setup step timed out while attempting to delete a pre-existing custom endpoint.");
-//         }
-//       } catch (DbClusterEndpointNotFoundException e){
-//           // Custom endpoint already does not exist - do nothing
-//       }
-//
-//       client.createDBClusterEndpoint((builder) ->
-//         builder.dbClusterEndpointIdentifier(customEndpointId)
-//             .dbClusterIdentifier(envInfo.getAuroraClusterName())
-//             .endpointType("ANY")
-//             .staticMembers(instances.get(0).getInstanceId()));
-//
-      long timeoutEndNano = System.nanoTime() + TimeUnit.MINUTES.toNanos(5);
-      String status = null;
+      // Delete pre-existing custom endpoints in case they weren't cleaned up in a previous run.
+      deleteEndpoints(client);
 
-      while (!"available".equals(status) && System.nanoTime() < timeoutEndNano) {
-        Filter customEndpointFilter =
-            Filter.builder().name("db-cluster-endpoint-type").values("custom").build();
-        DescribeDbClusterEndpointsResponse endpointsResponse = client.describeDBClusterEndpoints(
-            (builder) ->
-                builder.dbClusterEndpointIdentifier(customEndpointId).filters(customEndpointFilter));
+      List<TestInstanceInfo> instances = envInfo.getDatabaseInfo().getInstances();
+      String clusterId = envInfo.getAuroraClusterName();
 
-        List<DBClusterEndpoint> endpoints = endpointsResponse.dbClusterEndpoints();
-        if (endpoints.size() != 1) {
-          List<String> endpointURLs = endpoints.stream().map(DBClusterEndpoint::endpoint).collect(Collectors.toList());
-          throw new RuntimeException(
-              "Unexpected number of custom endpoints with endpoint identifier " + customEndpointId
-                  + " in region " + region + ". Expected 1, but found " + endpoints.size()
-                  + ". Endpoints: [" + endpointURLs + "].");
+      oneInstanceEndpointInfo = createEndpoint(
+          client, clusterId, oneInstanceEndpointId, instances.subList(0, 1));
+      twoInstanceEndpointInfo = createEndpoint(
+          client, clusterId, twoInstanceEndpointId, instances.subList(0, 2));
 
+      waitUntilEndpointsAvailable(client, clusterId);
+    }
+  }
+
+  private static void deleteEndpoints(RdsClient client) {
+    for (String endpointId : endpointIDs) {
+      try {
+        client.deleteDBClusterEndpoint((builder) -> builder.dbClusterEndpointIdentifier(endpointId));
+      } catch (DbClusterEndpointNotFoundException e) {
+        // Custom endpoint already does not exist - do nothing
+      }
+    }
+
+    waitUntilEndpointsDeleted(client);
+  }
+
+  private static void waitUntilEndpointsDeleted(RdsClient client) {
+    String clusterId = TestEnvironment.getCurrent().getInfo().getAuroraClusterName();
+    long deleteTimeoutNano = System.nanoTime() + TimeUnit.MINUTES.toNanos(5);
+    boolean allEndpointsDeleted = false;
+
+    while (!allEndpointsDeleted && System.nanoTime() < deleteTimeoutNano) {
+      Filter customEndpointFilter =
+          Filter.builder().name("db-cluster-endpoint-type").values("custom").build();
+      DescribeDbClusterEndpointsResponse endpointsResponse = client.describeDBClusterEndpoints(
+          (builder) ->
+              builder.dbClusterIdentifier(clusterId).filters(customEndpointFilter));
+      List<String> responseIDs = endpointsResponse.dbClusterEndpoints().stream()
+          .map(DBClusterEndpoint::dbClusterEndpointIdentifier).collect(Collectors.toList());
+
+      allEndpointsDeleted = endpointIDs.stream().noneMatch(responseIDs::contains);
+    }
+
+    if (!allEndpointsDeleted) {
+      throw new RuntimeException(
+          "The test setup step timed out while attempting to delete pre-existing test custom endpoints.");
+    }
+  }
+
+  private static CreateDbClusterEndpointResponse createEndpoint(
+      RdsClient client, String clusterId, String endpointId, List<TestInstanceInfo> instances) {
+    List<String> instanceIDs = instances.stream().map(TestInstanceInfo::getInstanceId).collect(Collectors.toList());
+    return client.createDBClusterEndpoint((builder) ->
+      builder.dbClusterEndpointIdentifier(endpointId)
+          .dbClusterIdentifier(clusterId)
+          .endpointType("ANY")
+          .staticMembers(instanceIDs));
+  }
+
+  public static void waitUntilEndpointsAvailable(RdsClient client, String clusterId) {
+    long timeoutEndNano = System.nanoTime() + TimeUnit.MINUTES.toNanos(5);
+    boolean allEndpointsAvailable = false;
+
+    while (!allEndpointsAvailable && System.nanoTime() < timeoutEndNano) {
+      Filter customEndpointFilter =
+          Filter.builder().name("db-cluster-endpoint-type").values("custom").build();
+      DescribeDbClusterEndpointsResponse endpointsResponse = client.describeDBClusterEndpoints(
+          (builder) ->
+              builder.dbClusterIdentifier(clusterId).filters(customEndpointFilter));
+      List<DBClusterEndpoint> endpoints = endpointsResponse.dbClusterEndpoints();
+
+      int availableEndpoints = 0;
+      for (int i = 0; i < endpoints.size() && availableEndpoints < endpointIDs.size(); i++) {
+        DBClusterEndpoint endpoint = endpoints.get(i);
+        if (endpointIDs.contains(endpoint.dbClusterEndpointIdentifier())
+            && "available".equals(endpoint.status())) {
+          availableEndpoints++;
         }
-
-        endpointInfo = endpoints.get(0);
-        status = endpointInfo.status();
       }
 
-      if (!"available".equals(status)) {
-        throw new RuntimeException(
-            "The test setup step timed out while waiting for the new custom endpoint to become available.");
-      }
+      allEndpointsAvailable = availableEndpoints == endpointIDs.size();
+    }
+
+    if (!allEndpointsAvailable) {
+      throw new RuntimeException(
+          "The test setup step timed out while waiting for the new custom endpoints to become available.");
     }
   }
 
   @BeforeEach
-  public void setUpEach() {
+  public void identifyWriter() {
     this.currentWriter =
         TestEnvironment.getCurrent().getInfo().getDatabaseInfo().getInstances().get(0)
             .getInstanceId();
   }
 
   @AfterAll
-  public static void deleteCustomEndpoints() {
-//     TestEnvironmentInfo envInfo = TestEnvironment.getCurrent().getInfo();
-//     String region = envInfo.getRegion();
-//     try (RdsClient client = RdsClient.builder().region(Region.of(region)).build()) {
-//       client.deleteDBClusterEndpoint((builder) -> builder.dbClusterEndpointIdentifier(customEndpointId));
-//     }
-
-    // TODO: do we need to wait until custom endpoints are fully deleted?
+  public static void cleanup() {
+    String region = TestEnvironment.getCurrent().getInfo().getRegion();
+    try (RdsClient client = RdsClient.builder().region(Region.of(region)).build()) {
+      deleteEndpoints(client);
+    }
   }
 
   @TestTemplate
@@ -154,13 +185,13 @@ public class CustomEndpointTest {
     try (final Connection conn =
              DriverManager.getConnection(
                  ConnectionStringHelper.getWrapperUrl(
-                     endpointInfo.endpoint(),
+                     oneInstanceEndpointInfo.endpoint(),
                      port,
                      TestEnvironment.getCurrent().getInfo().getDatabaseInfo().getDefaultDbName()),
                  props)) {
 
       String instanceId = auroraUtil.queryInstanceId(conn);
-      assertTrue(endpointInfo.staticMembers().contains(instanceId));
+      assertTrue(oneInstanceEndpointInfo.staticMembers().contains(instanceId));
 
       // Use failover API to break connection
       if (instanceId.equals(this.currentWriter)) {
@@ -172,7 +203,7 @@ public class CustomEndpointTest {
       assertThrows(FailoverSuccessSQLException.class, () -> auroraUtil.queryInstanceId(conn));
 
       String newInstanceId = auroraUtil.queryInstanceId(conn);
-      assertTrue(endpointInfo.staticMembers().contains(newInstanceId));
+      assertTrue(oneInstanceEndpointInfo.staticMembers().contains(newInstanceId));
     }
   }
 
