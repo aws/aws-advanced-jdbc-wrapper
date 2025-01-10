d43d6b5b53b8d1721fa4b5bd87b42e67f5172c5a
sergiyvamz <svvoshch@amazon.com>
2025-01-09 16:18:38 -0800

    adopt ReadWriteSplittingPlugin and AuroraInitialConnectionStrategyPlugin

--- wrapper/src/main/java/software/amazon/jdbc/plugin/readwritesplitting/ReadWriteSplittingPlugin.java
+++ wrapper/src/main/java/software/amazon/jdbc/plugin/readwritesplitting/ReadWriteSplittingPlugin.java
@@ -268,7 +268,7 @@ public class ReadWriteSplittingPlugin extends AbstractConnectionPlugin
   }
 
   private void getNewWriterConnection(final HostSpec writerHostSpec) throws SQLException {
-    final Connection conn = this.pluginService.connect(writerHostSpec, this.properties);
+    final Connection conn = this.pluginService.connect(writerHostSpec, this.properties, this);
     this.isWriterConnFromInternalPool = this.pluginService.isPooledConnectionProvider(writerHostSpec, this.properties);
     setWriterConnection(conn, writerHostSpec);
     switchCurrentConnectionTo(this.writerConnection, writerHostSpec);
@@ -495,7 +495,7 @@ public class ReadWriteSplittingPlugin extends AbstractConnectionPlugin
     for (int i = 0; i < connAttempts; i++) {
       HostSpec hostSpec = this.pluginService.getHostSpecByStrategy(HostRole.READER, this.readerSelectorStrategy);
       try {
-        conn = this.pluginService.connect(hostSpec, this.properties);
+        conn = this.pluginService.connect(hostSpec, this.properties, this);
         this.isReaderConnFromInternalPool = this.pluginService.isPooledConnectionProvider(hostSpec, this.properties);
         readerHost = hostSpec;
         break;
