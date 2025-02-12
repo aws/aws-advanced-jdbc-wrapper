f7ef88f5f7110d1696d2335e33d5c5b2668f4ba2
aaron-congo <aaron.congo@improving.com>
2025-02-12 13:46:22 -0800

    Error when passing Topology.class as the itemClass

---
+++ wrapper/src/main/java/software/amazon/jdbc/util/storage/Topology.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package software.amazon.jdbc.util.storage;
+
+import java.util.List;
+import software.amazon.jdbc.HostSpec;
+
+public class Topology {
+  private final List<HostSpec> hosts;
+
+  public Topology(List<HostSpec> hosts) {
+    this.hosts = hosts;
+  }
+
+  public List<HostSpec> getHosts() {
+    return hosts;
+  }
+}
