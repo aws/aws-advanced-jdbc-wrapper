2b88a7b6e1c37a91542ae8fb0978818f4c08f75d
sergiyvamz <svvoshch@amazon.com>
2025-11-05 12:36:24 -0800

    .

--- wrapper/src/test/java/integration/host/TestEnvironment.java
+++ wrapper/src/test/java/integration/host/TestEnvironment.java
@@ -1251,7 +1251,7 @@ public class TestEnvironment implements AutoCloseable {
   private static String getContainerBaseImageName(TestEnvironmentRequest request) {
     switch (request.getTargetJvm()) {
       case OPENJDK8:
-        return "14-ea-8-jdk-alpine3.10";
+        return "amazoncorretto:8-alpine";
       case OPENJDK11:
         return "amazoncorretto:11.0.19-alpine3.17";
       case OPENJDK17:
