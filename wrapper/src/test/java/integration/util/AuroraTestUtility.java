7b5e4cfa5bc5ceb6acc13e725f0c24a4eb8cc70a
sergiyvamz <75754709+sergiyvamz@users.noreply.github.com>
2026-05-08 13:48:24 -0700

    chore: test blue/green deployments clean up (#1904)

--- wrapper/src/test/java/integration/util/AuroraTestUtility.java
+++ wrapper/src/test/java/integration/util/AuroraTestUtility.java
@@ -144,6 +144,7 @@ import software.amazon.awssdk.services.rds.model.SwitchoverBlueGreenDeploymentRe
 import software.amazon.awssdk.services.rds.model.SwitchoverBlueGreenDeploymentResponse;
 import software.amazon.awssdk.services.rds.model.Tag;
 import software.amazon.awssdk.services.rds.waiters.RdsWaiter;
+import software.amazon.awssdk.utils.builder.SdkBuilder;
 import software.amazon.jdbc.util.DriverInfo;
 import software.amazon.jdbc.util.Pair;
 import software.amazon.jdbc.util.RdsUtils;
@@ -2411,6 +2412,40 @@ public class AuroraTestUtility {
           } catch (Exception ex) {
             LOGGER.warning(ex.getMessage());
           }
+        }
+      }
+    } catch (Exception ex) {
+      LOGGER.warning(ex.getMessage());
+    }
+  }
+
+  public void testBlueGreenDeploymentsCleanUp() {
+    try {
+      DescribeBlueGreenDeploymentsResponse response = rdsClient.describeBlueGreenDeployments(SdkBuilder::build);
+      for (BlueGreenDeployment bgd : response.blueGreenDeployments()) {
+        if (!bgd.blueGreenDeploymentName().startsWith("bgd-test-")) {
+          continue;
+        }
+        // Only delete deployments in a terminal or stable state.
+        if (!"available".equalsIgnoreCase(bgd.status())
+            && !"switchover_completed".equalsIgnoreCase(bgd.status())
+            && !"invalid_configuration".equalsIgnoreCase(bgd.status())) {
+          continue;
+        }
+        if (bgd.createTime() != null
+            && bgd.createTime().plus(12, ChronoUnit.HOURS).isAfter(Instant.now())) {
+          // Deployment was created less than 12 hours ago, do not delete.
+          continue;
+        }
+
+        LOGGER.finest("Deleting blue/green deployment " + bgd.blueGreenDeploymentName());
+        try {
+          rdsClient.deleteBlueGreenDeployment(
+              DeleteBlueGreenDeploymentRequest.builder()
+                  .blueGreenDeploymentIdentifier(bgd.blueGreenDeploymentIdentifier())
+                  .build());
+        } catch (Exception ex) {
+          LOGGER.warning(ex.getMessage());
         }
       }
     } catch (Exception ex) {
